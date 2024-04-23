6da1c9b210372ba3e43aa1788674df1b99f97286
gladiator <Gladiator>
2024-04-24 00:26:36 +0200

    24/04 night commitgit add proj.c

--- proj.c
+++ proj.c
@@ -8,16 +8,17 @@ int* stations; //pro kazdou zastavku, pro kazdeho lyzare na zastavce
 int where_bus;
 
 typedef struct route_t {
-	sem_t **can_get_on;//whether the bus is on the stop
+	sem_t *can_get_on;//whether the bus is on the stop
 	int *crowd;//ppl on each stop
+	int total_passengers_left;
 } Route;
 
 typedef struct bus_t {
 	int num_of_stations;
 	int *to_drop;
 	int *to_pick;
-	sem_t *capacity;
-	sem_t *can_start;
+	sem_t capacity;
+	sem_t can_start;
 } Bus;
 
 typedef struct pass_t {
@@ -28,13 +29,19 @@ typedef struct pass_t {
 
 void bus(Bus *skibus, Route *route)
 {
-	printf("A: BUS: started.\n");
-	for(int station = 0; station < skibus.num_of_stations || passengers_left == 0; station++)//PSEUDOCODE
+	while(route->total_passengers_left > 0)
 	{
-		sem_post(route->can_get_on[station]);
-		sem_wait(skibus->can_start);
-		sem_wait(route->can_get_on[station]);
-		printf("Going to station %d\n",station+1);
+		printf("A: BUS: started.\n");
+		for(int station = 0; station < skibus->num_of_stations; station++)//PSEUDOCODE
+		{
+			sem_post(&route->can_get_on[station]);
+			printf("WHOAMI\n");
+			sem_wait(&skibus->can_start);
+			sem_wait(&route->can_get_on[station]);
+			printf("ITSME\n");
+			sem_wait(&skibus->can_start);
+			printf("Going to station %d\n",station+1);
+		}
 	}
 	return;
 	//while(not all passengers)
@@ -53,14 +60,22 @@ void bus(Bus *skibus, Route *route)
 
 void passenger(Passenger *passenger, Bus *skibus, Route *route)
 {
-	printf("A: L %d: started\n", passenger->name);
+	printf("A: L %d: started, goes from %d to %d\n", passenger->name, passenger->from, passenger->to);
+	route->crowd[passenger->from-1]++;
 	//prijde na zastavku, stane do fronty
-	sem_wait(route->can_get_on[passenger->from]);//UPDATE WITH ROUTE!!
-	sem_wait(skibus->capacity);
+	sem_wait(&route->can_get_on[passenger->from-1]);//UPDATE WITH ROUTE!!
+	printf("%d is allowed in the bus", passenger->name);//log!
+	sem_wait(&skibus->capacity);
 	printf("A: L %d: boarding...", passenger->name);
-	route->crowd[passenger->from]--;//PSEUDOCODE!!
-	sem_post(skibus->capacity); 
-	sem_post(route->can_get_on[passenger->from]);//UPDATE WITH ROUTE!!
+	route->crowd[passenger->from-1]--;//PSEUDOCODE!!
+	route->total_passengers_left--;
+	if(route->crowd[passenger->from-1] == 0)
+	{
+		printf("LALALA\n");
+		sem_post(&skibus->can_start);
+	}
+	sem_post(&skibus->capacity); 
+	sem_post(&route->can_get_on[passenger->from-1]);
 	//pokud je bus na zastavce, nastoupi (SEMAFOR!)
 	//pokud je bus na zastavce vystoupi (SEMAFOR!)
 	//ukonci
@@ -70,15 +85,16 @@ void passenger(Passenger *passenger, Bus *skibus, Route *route)
 int main(int argc, char** argv)
 {
 	int check_pid;
-	int args[argc];
-	for(int arg = 1; arg <= argc; arg++)
+	int args[argc-1];
+	for(int arg = 1; arg <= argc-1; arg++)
 		args[arg-1] = atoi(argv[arg]);
 	check_pid = fork();
 	Bus skibus;
 	skibus.num_of_stations = args[1];
 	skibus.to_drop = (int*)malloc(args[1]*sizeof(int));
 	skibus.to_pick = (int*)malloc(args[1]*sizeof(int));
-	sem_init(skibus.capacity, 1, args[2]);
+	sem_init(&skibus.capacity, 1, args[2]);
+	sem_init(&skibus.can_start,1, 1);
 	for(int station = 0; station < args[1]; station++)
 	{
 		skibus.to_drop[station] = 0;
@@ -87,10 +103,11 @@ int main(int argc, char** argv)
 
 	Route route;
 	route.crowd = (int*)malloc(args[1]*sizeof(int));
-	route.can_get_on = (sem_t**)malloc(args[1]*sizeof(sem_t*));
+	route.can_get_on = (sem_t*)malloc(args[1]*sizeof(sem_t));
+	route.total_passengers_left = args[0];
 	for(int station = 0; station < args[1]; station++)
 	{
-		sem_init(route.can_get_on[station], 1, 0);
+		sem_init(&route.can_get_on[station], 1, 0);
 		route.crowd[station] = 0;
 	}
 
@@ -106,6 +123,7 @@ int main(int argc, char** argv)
 	for(int skiist = 1; skiist <= args[0]; skiist++)
 	{
 		check_pid = fork();
+		srand(getpid());
 		if(check_pid == 0)
 		{
 			Passenger skiist_waiting = {skiist, rand()%(args[2] - 1) + 1, 0};
